ca4c2637bdea684e17e3a5ea9f22e4fe3378bbed
bobgus39 <bobgus39@gmail.com>
2026-05-14 17:28:06 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import './i18n'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || ''
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
